bb5d6624bcbdb484cd20e8d864d65235fdd7b6d9
sergei <srjchsv@gmail.com>
2025-04-05 12:38:16 +0400

    feat: cache response

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { renderHtml } from "./renderHtml";
 
 export default {
-  async fetch(request, env) {
+  async fetch(request, env, ctx) {
+
+    const cache = caches.default;
+    const cacheKey = new Request(request.url);
+    const cached = await cache.match(cacheKey);
+
+    if (cached) return cached;
+
+
     const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
     const { results } = await stmt.all();
 
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
+    const response = new Response(renderHtml(JSON.stringify(results, null, 2)), {
       headers: {
         "content-type": "text/html",
+        'Cache-Control': 'public, max-age=300'
       },
     });
+
+    ctx.waitUntil(cache.put(cacheKey, response.clone()));
+    return response;
   },
 } satisfies ExportedHandler<Env>;
